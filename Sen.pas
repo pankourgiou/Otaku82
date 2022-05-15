8224380d5b6f0c805c55857c92f0a4c70e0bf59a
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-05-15 13:52:30 +0300

    Update and rename Sen.pas to sechszehn.py

--- Sen.pas
+++
@@ -1,3 +0,0 @@
-
-for _ in range(10):
-    print("All art is but imitation of nature.")
---
+++ sechszehn.py
@@ -0,0 +1,13 @@
+
+import time
+import antigravity
+
+start = time.time()
+print("十六 = 十六")
+
+yyz =  "十六 = 十六"
+
+print(bool(yyz))
+
+end = time.time()
+print(end - start)
